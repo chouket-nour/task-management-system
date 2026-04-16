29711f58a355fbf6b74de4368841c9b3dd9d1576
nour el houda chouket <nourelhouda.chouket@etudiant-isi.utm.tn>
2026-04-16 10:12:14 +0100

    chore: clean up tracked files

--- terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "vm_count"     { default = 5 }
-variable "vm_size"      { default = "Standard_DS2_v2" }
-variable "admin_user"   { default = "azureuser" }
-variable "location"     { default = "dc2"  }  
