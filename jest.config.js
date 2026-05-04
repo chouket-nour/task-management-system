6ff543d8fdb6cfebbc502fef34a602746c435e24
nour el houda chouket <nourelhouda.chouket@etudiant-isi.utm.tn>
2026-05-04 20:36:55 +0100

    fix: use a single root lcov and configure Sonar to use coverage/lcov.info

--- jest.config.js
+++ jest.config.js
@@ -8,21 +8,21 @@ const services = [
 ];
 
 module.exports = {
+  collectCoverage: true,
+  coverageReporters: ["lcov", "text"],
+  collectCoverageFrom: services.flatMap(service => [
+    `backend/${service}/**/*.js`,
+    `!backend/${service}/node_modules/**`,
+    `!backend/${service}/coverage/**`,
+    `!backend/${service}/jest.config.js`,
+    `!backend/${service}/server.js`,
+    `!backend/${service}/config/db.js`
+  ]),
+  coverageDirectory: "coverage",
   projects: services.map(service => ({
     displayName: service,
     testEnvironment: "node",
     rootDir: __dirname,
-    roots: [`<rootDir>/backend/${service}`],
-    collectCoverage: true,
-    collectCoverageFrom: [
-      `backend/${service}/**/*.js`,
-      "!**/node_modules/**",
-      "!**/coverage/**",
-      "!**/jest.config.js",
-      "!**/server.js",
-      "!**/config/db.js"
-    ],
-    coverageReporters: ["lcov", "text"],
-    coverageDirectory: `backend/${service}/coverage`
+    roots: [`<rootDir>/backend/${service}`]
   }))
 };
