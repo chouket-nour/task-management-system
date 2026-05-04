a5305cdd0553bf85c1849a4e5b3ad7bc5ba1dfc8
nour el houda chouket <nourelhouda.chouket@etudiant-isi.utm.tn>
2026-05-04 20:47:57 +0100

    fix: merge lcov coverage before SonarQube

--- jest.config.js
+++ jest.config.js
@@ -8,21 +8,20 @@ const services = [
 ];
 
 module.exports = {
-  collectCoverage: true,
-  coverageReporters: ["lcov", "text"],
-  collectCoverageFrom: services.flatMap(service => [
-    `backend/${service}/**/*.js`,
-    `!backend/${service}/node_modules/**`,
-    `!backend/${service}/coverage/**`,
-    `!backend/${service}/jest.config.js`,
-    `!backend/${service}/server.js`,
-    `!backend/${service}/config/db.js`
-  ]),
-  coverageDirectory: "coverage",
   projects: services.map(service => ({
     displayName: service,
     testEnvironment: "node",
     rootDir: __dirname,
-    roots: [`<rootDir>/backend/${service}`]
+    roots: [`<rootDir>/backend/${service}`],
+    collectCoverage: true,
+    collectCoverageFrom: [
+      `backend/${service}/**/*.js`,
+      `!backend/${service}/node_modules/**`,
+      `!backend/${service}/coverage/**`,
+      `!backend/${service}/server.js`,
+      `!backend/${service}/config/db.js`
+    ],
+    coverageReporters: ["lcov", "text"],
+    coverageDirectory: `backend/${service}/coverage`
   }))
 };
