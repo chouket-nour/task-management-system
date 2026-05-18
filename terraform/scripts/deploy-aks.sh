2cbf4f6bf7a9aee9ff849c3e6375f550c6d5c0e6
nour el houda chouket <nourelhouda.chouket@etudiant-isi.utm.tn>
2026-05-18 10:25:22 +0100

    fix: add output-directory and fix trailing backslash in deploy-aks.sh

--- terraform/scripts/deploy-aks.sh
+++ terraform/scripts/deploy-aks.sh
@@ -5,15 +5,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 API_MODEL="$SCRIPT_DIR/../modules/aks/apimodel.json"
 OUTPUT_DIR="$SCRIPT_DIR/../modules/aks/_output"
 
+echo "API_MODEL : $API_MODEL"
+echo "OUTPUT_DIR: $OUTPUT_DIR"
+
+mkdir -p "$OUTPUT_DIR"
+
 aks-engine-azurestack deploy \
-  --api-model       "$API_MODEL"         \
-  --location        "$AKS_LOCATION"      \
-  --resource-group  "$AKS_RG"            \
-  --subscription-id "$AKS_SUB_ID"        \
-  --client-id       "$AKS_CLIENT_ID"     \
-  --client-secret   "$AKS_CLIENT_SECRET" \
-  --auth-method     client_secret        \
-  --azure-env       AzureStackCloud      \
-  --identity-system adfs                 \
-  --force-overwrite                      \
+  --api-model        "$API_MODEL"         \
+  --location         "$AKS_LOCATION"      \
+  --resource-group   "$AKS_RG"            \
+  --subscription-id  "$AKS_SUB_ID"        \
+  --client-id        "$AKS_CLIENT_ID"     \
+  --client-secret    "$AKS_CLIENT_SECRET" \
+  --auth-method      client_secret        \
+  --azure-env        AzureStackCloud      \
+  --identity-system  adfs                 \
+  --force-overwrite                       \
   --output-directory "$OUTPUT_DIR"
